d3c5b25b96c4a9d65059284d29a73161bff67e64
Evan Rybovic <rybovic.evan@gmail.com>
2021-04-22 23:31:50 -0700

    Added sign up and log in functionality

---
+++ Pages/UserAction/LogIn.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PersonalSite.Models;
+
+using static BCrypt.Net.BCrypt;
+
+namespace PersonalSite.Pages.UserAction
+{
+    public class LogInModel : PageModel
+    {
+        [BindProperty]
+        public User User { get; set; }
+
+        private readonly PersonalSite.Data.PersonalSiteContext _context;
+
+        public LogInModel(PersonalSite.Data.PersonalSiteContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var result = await _context.User.Where(i => i.Username == Request.Form["username"].ToString()).ToArrayAsync();
+
+            // TODO add username invalid fail
+            if (result.Length == 0)
+                return Page();
+
+            User = result[0];
+            if (Verify(Request.Form["password"].ToString(), User.Password))
+            {
+                return RedirectToPage("../Index");
+            }
+            else
+            {
+                // TODO add password inccorect fail
+                return Page();
+            } 
+        }
+
+        private bool ComparePasswords(string dbPassword, string clientPassword)
+        {
+            return dbPassword.Equals(HashPassword(clientPassword));
+        }
+    }
+}
