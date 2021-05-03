3be590ba7d217f5e5c6911c08a47a2792621f67c
Evan Rybovic <rybovic.evan@gmail.com>
2021-05-02 23:18:57 -0700

    Temp and Humid integration
    - Added new labels for Temp and Humidity.
    - Temp and Humidity updates now update the labels and will no longer append the list.
    - Added new methods in ChatHub and the chat.js files to request the most recent Temp and Humid data when the connection to SignalR is made instead of waiting up to 5 seconds for the MQTT message to arrive.

--- MQTT/MQTTListener.cs
+++ MQTT/MQTTListener.cs
@@ -12,19 +12,32 @@ namespace PersonalSite.MQTT
     {
         private IMqttClient client;
 
+        private static double MostRecentTemp = 0;
+        private static double MostRecentHumid = 0;
+
         public MQTTListener(IHubContext<ChatHub> hubContext)
         {
             var factory = new MqttFactory();
             client = factory.CreateMqttClient();
-            var options = new MqttClientOptionsBuilder()
-            .WithTcpServer("localhost", 1883);
+            var options = new MqttClientOptionsBuilder().WithTcpServer("localhost", 1883);
 
             client.ConnectAsync(options.Build());
 
             client.UseApplicationMessageReceivedHandler(async e =>
             {   
                 Console.WriteLine($"Recieved on topic: {e.ApplicationMessage.Topic}");
-                await hubContext.Clients.All.SendAsync("ReceiveMessage", "Arduino", Encoding.UTF8.GetString(e.ApplicationMessage.Payload));
+                if (e.ApplicationMessage.Topic.Contains("temp"))
+                {
+                    MostRecentTemp = Math.Round(Double.Parse(Encoding.UTF8.GetString(e.ApplicationMessage.Payload)), 2);
+                    await hubContext.Clients.All.SendAsync("ReceiveTemp", MostRecentTemp.ToString());
+                }
+                else if (e.ApplicationMessage.Topic.Contains("humid"))
+                {
+                    MostRecentHumid = Math.Round(Double.Parse(Encoding.UTF8.GetString(e.ApplicationMessage.Payload)), 2);
+                    await hubContext.Clients.All.SendAsync("ReceiveHumid", MostRecentHumid.ToString());
+                }
+                else
+                    await hubContext.Clients.All.SendAsync("ReceiveMessage", "Arduino", Encoding.UTF8.GetString(e.ApplicationMessage.Payload));
             });
 
             client.UseConnectedHandler(async e => 
@@ -35,6 +48,18 @@ namespace PersonalSite.MQTT
                 await client.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic("/response/humid").Build());
                 Console.WriteLine("Listener has subscribed to topics");
             });
+        }
+
+        // TODO I feel like this isn't the best way to do this
+        public static double GetMostRecentTemp()
+        {
+            return MostRecentTemp;
+        }
+
+        // TODO I feel like this isn't the best way to do this
+        public static double GetMostRecentHumid()
+        {
+            return MostRecentHumid;
         }
     }
 }
